d039b3e11589faf778295a9bc5acc7f70e6643da
Eric <heyericbai@gmail.com>
2016-06-29 15:25:16 -0400

    forgot to preserve the import statement to allow the script to execute

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import './models/custom-inflector-rules';
+import 'npm:inobounce'; // import to allow enable detection inside script to run
 
 let App;
 
