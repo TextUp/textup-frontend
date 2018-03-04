ed7f54af3cfb7e7ff451d4e82a2b007ab93bbf2e
Eric Bai <eric@ericbai.com>
2018-03-04 17:52:39 -0500

    fixed travis config typo, moved travis-related scripts to own folder

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,4 +6,4 @@ app_path=$1
 bucket_name=$2
 
 aws s3 sync ${app_path} s3://${bucket_name} --only-show-errors --size-only --delete --exclude "index.html"
-aws s3 sync ${app_path} s3://${bucket_name} --only-show-errors --size-only --exclude "*" --include "index.html" --cache-control 'max-age=180'
+aws s3 sync ${app_path} s3://${bucket_name} --only-show-errors --size-only --exclude "*" --include "index.html" --cache-control "max-age=180"
