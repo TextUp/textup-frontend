8f27123e91b42851c6e68658d823c5708bf62f4d
Eric Bai <eric@ericbai.com>
2018-03-04 18:36:18 -0500

    fixed bug in deploy script that caused the index file to not be deployed

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,5 +5,11 @@
 app_path=$1
 bucket_name=$2
 
-aws s3 sync ${app_path} s3://${bucket_name} --only-show-errors --size-only --delete --exclude "index.html"
-aws s3 sync ${app_path} s3://${bucket_name} --only-show-errors --size-only --exclude "*" --include "index.html" --cache-control "max-age=180"
+aws s3 sync ${app_path} s3://${bucket_name} --size-only --delete --exclude "index.html"
+
+# don't want the size-only flag when syncing index html because this file is
+# likely does NOT have a has behind it and is likely the same size. Therefore, if we
+# added in the `--size-only` flag, this file would not be synchronized even though the
+# previous assets have been deleted, leading to a broken app where the html file points
+# to older assets that have already been deleted
+aws s3 sync ${app_path} s3://${bucket_name} --exclude "*" --include "index.html" --cache-control "max-age=180"
