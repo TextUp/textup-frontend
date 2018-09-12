5f3da848e17314c091eb96e41d63f9e6e8ed8c42
Eric Bai <eric@ericbai.com>
2018-09-12 17:37:41 -0400

    fixed invalid number validation tokens still succeeding in storing the number

--- app/services/staff-service.js
+++ app/services/staff-service.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import config from 'textup-frontend/config/environment';
 import { format } from 'textup-frontend/utils/phone-number';
 
+const { RSVP } = Ember;
+
 export default Ember.Service.extend({
   authService: Ember.inject.service(),
   dataService: Ember.inject.service(),
@@ -26,32 +28,40 @@ export default Ember.Service.extend({
   },
 
   startVerifyPersonalPhone(num) {
-    return this.get('authService')
-      .authRequest({
-        type: 'POST',
-        url: `${config.host}/v1/numbers`,
-        data: JSON.stringify({ phoneNumber: num })
-      })
-      .then(
-        () => this.get('notifications').info(`Sent verification text to ${format(num)}`),
-        this.get('dataService').buildErrorHandler()
-      );
+    return new RSVP.Promise((resolve, reject) => {
+      this.get('authService')
+        .authRequest({
+          type: 'POST',
+          url: `${config.host}/v1/numbers`,
+          data: JSON.stringify({ phoneNumber: num })
+        })
+        .then(success => {
+          this.get('notifications').info(`Sent verification text to ${format(num)}`);
+          resolve(success);
+        }, this.get('dataService').buildErrorHandler(reject));
+    });
   },
   finishVerifyPersonalPhone(num, validationCode) {
-    const notifications = this.get('notifications');
-    return this.get('authService')
-      .authRequest({
-        type: 'POST',
-        url: `${config.host}/v1/numbers`,
-        data: JSON.stringify({ phoneNumber: num, token: validationCode })
-      })
-      .then(
-        () => notifications.success(`Successfully validated ${format(num)}`),
-        failure => {
-          if (this.get('dataService').displayErrors(failure) === 0) {
-            notifications.error(`Invalid or expired token for ${format(num)}`);
+    return new RSVP.Promise((resolve, reject) => {
+      const notifications = this.get('notifications');
+      this.get('authService')
+        .authRequest({
+          type: 'POST',
+          url: `${config.host}/v1/numbers`,
+          data: JSON.stringify({ phoneNumber: num, token: validationCode })
+        })
+        .then(
+          success => {
+            notifications.success(`Successfully validated ${format(num)}`);
+            resolve(success);
+          },
+          failure => {
+            if (this.get('dataService').displayErrors(failure) === 0) {
+              notifications.error(`Invalid or expired token for ${format(num)}`);
+            }
+            reject(failure);
           }
-        }
-      );
+        );
+    });
   }
 });
