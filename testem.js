593fe2805d3c00b664344784c9bc2e70caa57b9c
Eric Bai <eric@ericbai.com>
2018-09-11 21:44:49 -0400

    consolidated and updated testem test runner config

--- testem.js
+++ testem.js
@@ -3,6 +3,15 @@ module.exports = {
   framework: 'qunit',
   test_page: 'tests/index.html',
   disable_watching: true,
-  launch_in_ci: ['PhantomJS'],
-  launch_in_dev: ['Chrome']
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome'],
+  // from https://embermap.com/notes/59-ember-tests-with-headless-chrome
+  // and https://github.com/testem/testem/blob/master/docs/browser_args.md
+  browser_args: {
+    Chrome: {
+      all: ['--no-default-browser-check'],
+      dev: ['--auto-open-devtools-for-tabs'],
+      ci: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--window-size=1440,900']
+    }
+  }
 };
