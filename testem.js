9c351f5b5e2e0e3b5b1db11f2d6d4d82eaecbace
Eric Bai <eric@ericbai.com>
2018-09-11 22:11:09 -0400

    debugging travis and testem configs

--- testem.js
+++ testem.js
@@ -1,17 +1,17 @@
 /*jshint node:true*/
 module.exports = {
   framework: 'qunit',
-  test_page: 'tests/index.html',
+  test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
-  // from https://embermap.com/notes/59-ember-tests-with-headless-chrome
-  // and https://github.com/testem/testem/blob/master/docs/browser_args.md
+  launch_in_dev: [],
+  // from https://embermap.com/notes/64-our-testem-config
   browser_args: {
-    Chrome: {
-      all: ['--no-default-browser-check'],
-      dev: ['--auto-open-devtools-for-tabs'],
-      ci: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--window-size=1440,900']
-    }
+    Chrome: [
+      '--headless',
+      '--disable-gpu',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ]
   }
 };
