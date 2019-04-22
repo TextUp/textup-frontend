6823c288378b9d19ff2fa5cdd26c326892930989
Dao Han Lim <dao_han_lim@brown.edu>
2019-04-22 12:14:50 -0400

    added id to contact list, and updated task-data to match

--- app/data/task-data.js
+++ app/data/task-data.js
@@ -4,8 +4,8 @@ export default [
     title: 'Add a contact',
     text:
       'Before you can message clients, you have to add a contact. Click the Add Contact button and add our Customer Support Line as a contact.',
-    elementsToPulse: ['#tour-openContactListButton'],
-    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-addNewContactButton']
+    elementsToPulse: ['#tour-openNewContactButton'],
+    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-addNewContactButton'],
   },
   {
     id: 'sendMessage',
@@ -13,7 +13,7 @@ export default [
     text:
       "Now let's send a text. Click the Compose button. If you don't know who to message, you can message the Customer Support contact you just added.",
     elementsToPulse: ['#tour-openMessageButton'],
-    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openMessageButtonMobile']
+    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openMessageButtonMobile'],
   },
   {
     id: 'makeCall',
@@ -21,7 +21,7 @@ export default [
     text:
       "You can also make calls through TextUp. These calls will appear on caller ID as coming from your TextUp phone number-- even if you're accessing TextUp on a mobile phone. Click the Call button.",
     elementsToPulse: ['#tour-openCallButton'],
-    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openCallButtonMobile']
+    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openCallButtonMobile'],
   },
   {
     id: 'setAvailability',
@@ -29,7 +29,7 @@ export default [
     text:
       'You can choose how and how often you want to be notified of incoming calls and texts to your TextUp phone number. Customize your availability schedule, away message, voicemail, and more by clicking the Availability button',
     elementsToPulse: ['#tour-openAvailabilityButton'],
-    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openAvailabilityButtonMobile']
+    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openAvailabilityButtonMobile'],
   },
   {
     id: 'createTag',
@@ -37,7 +37,7 @@ export default [
     text:
       "You can also send the same message to multiple clients at once by putting them in a tag. These are not group messages; it's the same message being sent to many people individually.  Create a tag by clicking the Tag button.",
     elementsToPulse: ['#tour-openTagsListButton', '#tour-createNewTagButton'],
-    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-createNewTagButtonMobile']
+    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-createNewTagButtonMobile'],
   },
   {
     id: 'exportMessage',
@@ -45,7 +45,7 @@ export default [
     text:
       'You can export your TextUp records to a PDF by clicking the Export button. You can export records for one or many clients at once.',
     elementsToPulse: ['#tour-openExportButton'],
-    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openExportButtonMobile']
+    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openExportButtonMobile'],
   },
   {
     id: 'additionalActions',
@@ -53,9 +53,13 @@ export default [
     text:
       'You can supplement client records with case notes (text notes, images, or gps locations), schedule messages to send to clients in the future, and more by selecting a contact and then pressing the + button next to the text bar.',
     elementsToPulse: [
+      '#tour-openContactListButton',
       '#tour-contactsList .result-item:first-child .entity-display__body > :first-child',
-      '.record-actions-control__dropdown-trigger'
+      '.record-actions-control__dropdown-trigger',
     ],
-    elementsToPulseMobile: ['#tour-openSlideoutButton', '#tour-openExportButtonMobile']
-  }
+    elementsToPulseMobile: [
+      '#tour-contactsList .result-item:first-child .entity-display__body > :first-child',
+      '.record-actions-control__dropdown-trigger',
+    ],
+  },
 ];
